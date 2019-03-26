ecaefa89f02d66b11b301dcea3f578539126f6c9
Kondr <Kondr@ANDROMEDA>
2019-03-26 17:51:34 +0300

    Представление студентов в web интерфейсе

---
+++ Education.WEB/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Education.DAL.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Education.WEB.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly EducationDB _DB;
+
+        public StudentsController(EducationDB db)
+        {
+            _DB = db;
+        }
+
+        public IActionResult Index()
+        {
+            var group = _DB.StudentGroups.Include(g => g.Students).First();
+
+            return View(group);
+        }
+    }
+}
