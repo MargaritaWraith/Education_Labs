10d66af68f398eb692daf9edb6162d5d7be311e5
Kondr <Kondr@ANDROMEDA>
2019-03-27 12:54:10 +0300

    Добавлено редактирование списка групп

--- Education.WEB/Controllers/StudentsController.cs
+++ Education.WEB/Controllers/StudentsController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Education.DAL.Context;
+using Education.Entityes.EF;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -26,13 +28,48 @@ namespace Education.WEB.Controllers
 
         public IActionResult GroupStudents(int Id)
         {
-            var group = _DB.StudentGroups.Include(g => g.Students).FirstOrDefault(g=>g.Id == Id);
-            if (group ==null)
+            var group = _DB.StudentGroups.Include(g => g.Students).FirstOrDefault(g => g.Id == Id);
+            if (group == null)
                 return NotFound();
 
             return View(group);
         }
 
+        public IActionResult EditGroup(int? Id)
+        {
+            StudentGroup group;
+            if (Id == null)
+                @group = new StudentGroup();
+            else
+            {
+                group = _DB.StudentGroups.FirstOrDefault(g => g.Id == Id);
+                if (group == null)
+                    return NotFound();
+            }
+
+            return View(group);
+        }
+
+        [HttpPost]
+        public IActionResult EditGroup(StudentGroup group)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(group);
+            }
+
+            if (group.Id == 0) _DB.StudentGroups.Add(group);
+            else
+            {
+                var db_group = _DB.StudentGroups.FirstOrDefault(g => g.Id == group.Id);
+                if (db_group==null)
+                    return NotFound();
+                db_group.Name = group.Name;
+            }
+
+            _DB.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 
 }
