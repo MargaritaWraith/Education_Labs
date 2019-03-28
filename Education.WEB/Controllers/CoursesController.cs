c56516c70a411a74de146ed22bdd5941eba15f57
Kondr <Kondr@ANDROMEDA>
2019-03-28 18:42:14 +0300

    Добавлен вывод преподавателей

---
+++ Education.WEB/Controllers/CoursesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Education.DAL.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Education.WEB.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly EducationDB _DB;
+        public CoursesController(EducationDB db)
+        {
+            _DB = db;
+        }
+
+        public IActionResult Index()
+        {
+            var courses = _DB.Courses.ToArray();
+
+            return View(courses);
+        }
+    }
+}
---
+++ Education.WEB/Controllers/LectorsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Education.DAL.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Education.WEB.Controllers
+{
+    public class LectorsController : Controller
+    {
+        private readonly EducationDB _DB;
+
+        public LectorsController(EducationDB db)
+        {
+            _DB = db;
+        }
+
+        public IActionResult Index()
+        {
+            var lectors = _DB.Lectors.ToArray();
+
+            return View(lectors);
+        }
+    }
+}
