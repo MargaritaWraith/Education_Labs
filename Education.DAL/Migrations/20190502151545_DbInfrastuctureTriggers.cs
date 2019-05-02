4112b406026c2bb826a2aee21b7e3390549bb7b2
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-05-02 18:20:39 +0300

    Триггер на таблицу Выполнений лабораторных работ dbo.StudentsLabWorks

---
+++ Education.DAL/Migrations/20190502151545_DbInfrastuctureTriggers.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Education.DAL.Migrations
+{
+    public partial class DbInfrastuctureTriggers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE TRIGGER [dbo].[RatingTrigger]
+	ON [dbo].[StudentsLabWorks]
+	AFTER INSERT, UPDATE 
+AS
+
+IF EXISTS (SELECT Rating FROM [dbo].[StudentsLabWorks]
+			WHERE Rating < 1 OR Rating > 5)
+
+BEGIN
+	SET NOCOUNT ON;
+	RAISERROR ('Недопустимое значение оценки',16,1);
+	ROLLBACK TRANSACTION;  
+	RETURN
+END
+");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP TRIGGER [dbo].[RatingTrigger];");
+        }
+    }
+}
